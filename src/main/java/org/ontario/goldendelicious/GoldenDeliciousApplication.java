ddcff614bc292c8ee8355ed1c07670158a96dc6f
Bohdan Herashchenko <b.gerashchencko@gmail.com>
2018-06-21 15:11:42 +0300

    Build Spring-Boot application

---
+++ src/main/java/org/ontario/goldendelicious/GoldenDeliciousApplication.java
@@ -0,0 +1,12 @@
+package org.ontario.goldendelicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenDeliciousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenDeliciousApplication.class, args);
+    }
+}
