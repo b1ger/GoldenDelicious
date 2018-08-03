452cfee35f7a5290cc51257ec3686df7603254af
Bohdan Herashchenko <b.gerashchencko@gmail.com>
2018-08-03 16:33:39 +0300

    Add schema of db

---
+++ src/main/java/org/ontario/goldendelicious/domain/PatientCard.java
@@ -0,0 +1,26 @@
+package org.ontario.goldendelicious.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class PatientCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Patient patient;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "card")
+    private List<Record> records;
+
+}
