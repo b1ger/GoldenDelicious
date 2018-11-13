4980c9f9cea7c66ac08d6f8d4d1446792b980c29
Bohdan Herashchenko <b.gerashchencko@gmail.com>
2018-11-13 19:24:57 +0200

    Admin :: Edit staff table, change field date

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -3,25 +3,25 @@ CREATE DATABASE dantist_dev;
 CREATE DATABASE dantist_prod;
 
 #Create database service accounts
-CREATE USER  'dantist_dev_user'@'localhost' IDENTIFIED BY 'ontario';
-CREATE USER  'dantist_prod_user'@'localhost' IDENTIFIED BY 'ontario';
-CREATE USER  'dantist_dev_user'@'%' IDENTIFIED BY 'ontario';
-CREATE USER  'dantist_prod_user'@'%' IDENTIFIED BY 'ontario';
+CREATE USER  'dentist_dev_user'@'localhost' IDENTIFIED BY 'ontario';
+CREATE USER  'dentist_prod_user'@'localhost' IDENTIFIED BY 'ontario';
+CREATE USER  'dentist_dev_user'@'%' IDENTIFIED BY 'ontario';
+CREATE USER  'dentist_prod_user'@'%' IDENTIFIED BY 'ontario';
 
 #Database grants
-GRANT SELECT ON dantist_dev.* to 'dantist_dev_user'@'localhost';
-GRANT INSERT ON dantist_dev.* to 'dantist_dev_user'@'localhost';
-GRANT UPDATE ON dantist_dev.* to 'dantist_dev_user'@'localhost';
-GRANT DELETE ON dantist_dev.* to 'dantist_dev_user'@'localhost';
-GRANT SELECT ON dantist_prod.* to 'dantist_prod_user'@'localhost';
-GRANT INSERT ON dantist_prod.* to 'dantist_prod_user'@'localhost';
-GRANT UPDATE ON dantist_prod.* to 'dantist_prod_user'@'localhost';
-GRANT DELETE ON dantist_prod.* to 'dantist_prod_user'@'localhost';
-GRANT SELECT ON dantist_dev.* to 'dantist_dev_user'@'%';
-GRANT INSERT ON dantist_dev.* to 'dantist_dev_user'@'%';
-GRANT UPDATE ON dantist_dev.* to 'dantist_dev_user'@'%';
-GRANT DELETE ON dantist_dev.* to 'dantist_dev_user'@'%';
-GRANT SELECT ON dantist_prod.* to 'dantist_prod_user'@'%';
-GRANT INSERT ON dantist_prod.* to 'dantist_prod_user'@'%';
-GRANT UPDATE ON dantist_prod.* to 'dantist_prod_user'@'%';
-GRANT DELETE ON dantist_prod.* to 'dantist_prod_user'@'%';
+GRANT SELECT ON dantist_dev.* to 'dentist_dev_user'@'localhost';
+GRANT INSERT ON dantist_dev.* to 'dentist_dev_user'@'localhost';
+GRANT UPDATE ON dantist_dev.* to 'dentist_dev_user'@'localhost';
+GRANT DELETE ON dantist_dev.* to 'dentist_dev_user'@'localhost';
+GRANT SELECT ON dantist_prod.* to 'dentist_prod_user'@'localhost';
+GRANT INSERT ON dantist_prod.* to 'dentist_prod_user'@'localhost';
+GRANT UPDATE ON dantist_prod.* to 'dentist_prod_user'@'localhost';
+GRANT DELETE ON dantist_prod.* to 'dentist_prod_user'@'localhost';
+GRANT SELECT ON dantist_dev.* to 'dentist_dev_user'@'%';
+GRANT INSERT ON dantist_dev.* to 'dentist_dev_user'@'%';
+GRANT UPDATE ON dantist_dev.* to 'dentist_dev_user'@'%';
+GRANT DELETE ON dantist_dev.* to 'dentist_dev_user'@'%';
+GRANT SELECT ON dantist_prod.* to 'dentist_prod_user'@'%';
+GRANT INSERT ON dantist_prod.* to 'dentist_prod_user'@'%';
+GRANT UPDATE ON dantist_prod.* to 'dentist_prod_user'@'%';
+GRANT DELETE ON dantist_prod.* to 'dentist_prod_user'@'%';
