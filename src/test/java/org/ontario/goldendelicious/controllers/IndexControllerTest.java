445a64ca55209db8d35ce347710aaa4f7071b6f2
Bogdan Herashchenko <bohdan.herashchenko@volia.com>
2018-12-05 15:49:50 +0200

    Service request :: Add action for saving new Request with tests.

--- src/test/java/org/ontario/goldendelicious/controllers/IndexControllerTest.java
+++ src/test/java/org/ontario/goldendelicious/controllers/IndexControllerTest.java
@@ -4,9 +4,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.ontario.goldendelicious.domain.Request;
 import org.ontario.goldendelicious.domain.Staff;
+import org.ontario.goldendelicious.domain.enums.ServiceType;
 import org.ontario.goldendelicious.domain.enums.StaffType;
+import org.ontario.goldendelicious.repositories.RequestRepository;
 import org.ontario.goldendelicious.repositories.StaffRepository;
+import org.ontario.goldendelicious.services.RequestServiceImpl;
 import org.ontario.goldendelicious.services.StaffServiceImpl;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -15,9 +19,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class IndexControllerTest {
@@ -31,10 +37,16 @@ public class IndexControllerTest {
     @Mock
     private StaffRepository staffRepository;
 
+    @Mock
+    private RequestServiceImpl requestService;
+
+    @Mock
+    private RequestRepository requestRepository;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        this.controller = new IndexController(staffService);
+        this.controller = new IndexController(staffService, requestService);
         this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
     }
@@ -53,6 +65,39 @@ public class IndexControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("request"))
                 .andExpect(model().attributeExists("doctors"))
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void shouldApplyRequestForm() throws Exception {
+        Request request = new Request();
+        request.setId(1L);
+
+        mockMvc.perform(post("/index/request")
+                .param("firstName", "FirstName")
+                .param("lastName", "LastName")
+                .param("phone", "093-429-45-89")
+                .param("email", "example@mail.com")
+                .param("serviceType", String.valueOf(ServiceType.SERVICE_1))
+                .param("doctorId", "1458967")
+                .param("date", "10 SEP 1998")
+                .param("time", "23:59")
+        )
+                .andExpect(status().isOk());
+
+        when(requestRepository.save(any())).thenReturn(request);
+
+        verify(requestService, times(1)).save(any());
+    }
+
+    @Test
+    public void shouldGetErrorsFromRequestForm() throws Exception {
+
+        mockMvc.perform(post("/index/request")
+                .param("firstName", "FirstName")
+                .param("lastName", "LastName")
+        )
+                .andExpect(status().isOk())
                 .andExpect(view().name("index"));
     }
 }
